1592f6e7c24af82acc884621594318ee9f203814
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-07-03 21:33:33 +0300

    add profile

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { Profile } from './components/Profile/Profile';
+import { FriendList } from './components/FriendList/FriendList';
+import { Statistics } from './components/Statistics/Statistics';
+import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
+
+import user from './components/data/user.json';
+import data from './components/data/data.json';
+import friends from './components/data/friends.json';
+import transactions from './components/data/transactions.json';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} /> */}
+      {/* <FriendList friends={friends} /> */}
+      {/* <TransactionHistory items={transactions} /> */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,45 @@
+import propTypes from 'prop-types';
+import styles from './styles.module.css';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
+
+      <ul className={styles.stats}>
+        <li className={styles.item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>
+            {stats.views.toLocaleString()}
+          </span>
+        </li>
+        <li className={styles.item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: propTypes.string.isRequired,
+  avatar: propTypes.string.isRequired,
+  tag: propTypes.string.isRequired,
+  location: propTypes.string.isRequired,
+  stats: propTypes.string.isRequired,
+  followers: propTypes.number.isRequired,
+  views: propTypes.number.isRequired,
+  likes: propTypes.number.isRequired,
+};
+
+export default Profile;
