4a9c6f04a4c2d753918656fbb800ee49bbf9bfcf
OlgaDudnik <olga.dudnik.ua@gmail.com>
2022-07-03 21:39:26 +0300

    add profile

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,28 @@
-import propTypes from 'prop-types';
-import styles from './styles.module.css';
+// import propTypes from 'prop-types';
+// import styles from './styles.module.css';
 
-<section class="statistics">
-  <h2 class="title">Upload stats</h2>
+// <section class="statistics">
+//   <h2 class="title">Upload stats</h2>
 
-  <ul class="stat-list">
-    <li class="item">
-      <span class="label">.docx</span>
-      <span class="percentage">4%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp3</span>
-      <span class="percentage">14%</span>
-    </li>
-    <li class="item">
-      <span class="label">.pdf</span>
-      <span class="percentage">41%</span>
-    </li>
-    <li class="item">
-      <span class="label">.mp4</span>
-      <span class="percentage">12%</span>
-    </li>
-  </ul>
-</section>;
+//   <ul class="stat-list">
+//     <li class="item">
+//       <span class="label">.docx</span>
+//       <span class="percentage">4%</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">.mp3</span>
+//       <span class="percentage">14%</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">.pdf</span>
+//       <span class="percentage">41%</span>
+//     </li>
+//     <li class="item">
+//       <span class="label">.mp4</span>
+//       <span class="percentage">12%</span>
+//     </li>
+//   </ul>
+// </section>;
 
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215)
